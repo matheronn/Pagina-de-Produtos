a1b007cdd13cdc14980408c9c4605aa844312355
Matheus <matheusem94@gmail.com>
2024-08-28 19:49:22 -0300

    Feat: add HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="container h-[70vh]">
+      <h1 className="text-center font-bold text-3xl">HomePage</h1>
+
+      <h2 className="text-center font-bold text-2xl mt-10">
+        Conteudo fictício Para home
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum
+        doloremque possimus repellat eius ducimus, itaque nostrum animi nobis
+        laboriosam, debitis minus distinctio neque eveniet nemo! Fugiat
+        reprehenderit tempore incidunt. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Assumenda voluptatibus ea mollitia laborum maiores
+        repellendus recusandae incidunt beatae nemo velit, rerum nulla pariatur
+        natus nobis. Esse mollitia nihil et. Recusandae? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Vero cupiditate expedita illum
+        dolorem, numquam saepe quaerat. Nulla, ut ad. Maxime sed, suscipit
+        ducimus molestias ad eligendi assumenda praesentium nulla eos.
+      </p>
+    </div>
+  );
+}
