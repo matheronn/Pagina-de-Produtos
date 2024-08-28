3e95639450855d9a786db4a14eb261ff9ba3049b
Matheus <matheusem94@gmail.com>
2024-08-28 19:07:27 -0300

    feat: New header Layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,64 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Header() {
+const Header = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
-    <header className='bg-yellow-100'>
-      <div className='container flex justify-between'>
-        {/* <img src="" alt="" /> */}
-        <p>logo</p>
+    <header className="bg-black text-white p-4">
+      <div className="container flex items-center justify-between">
+          <Link to="/">
+            <img src="/lv.png" alt="" width="100"/>
+          </Link>
         <nav>
-					<ul className="flex gap-4">
-						<li>Home</li>
-						<li>Produtos</li>
-						<li>Contato</li>
-					</ul>
+          <ul className="flex space-x-4 items-center">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
+            <li
+              className="relative"
+              onMouseEnter={toggleDropdown}
+              onMouseLeave={toggleDropdown}
+            >
+              <button className="focus:outline-none">Categorias</button>
+              {dropdownOpen && (
+                <ul className="absolute bg-white text-gray-800 shadow-lg right-0 rounded mt-0 w-48">
+                  <li className="hover:bg-gray-200">
+                    <Link to="/category/jewelery" className="block px-4 py-2">
+                      Joias
+                    </Link>
+                  </li>
+                  <li className="hover:bg-gray-200">
+                    <Link
+                      to="/category/mens-clothing"
+                      className="block px-4 py-2"
+                    >
+                      Roupas Masculinas
+                    </Link>
+                  </li>
+                  <li className="hover:bg-gray-200">
+                    <Link
+                      to="/category/electronics"
+                      className="block px-4 py-2"
+                    >
+                      Eletronicos
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+          </ul>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
+
+export default Header;
