c12a25c72e910148644dd81efa152b73866876ed
Matheus <matheusem94@gmail.com>
2024-08-27 23:07:49 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='w-full'>
+      <div className='container max-w-'>test</div>
+    </header>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      maxWidth: {
+        'screen-sm': '640px',
+        'screen-md': '768px',
+        'screen-lg': '1024px',
+        'screen-xl': '1280px',
+        'screen-2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
+
