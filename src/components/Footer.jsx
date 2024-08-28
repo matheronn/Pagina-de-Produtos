3f43399bc7d5b1592aca366986c49c746cbb08f0
Matheus <matheusem94@gmail.com>
2024-08-28 19:23:47 -0300

    Refactor: Footer indentation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer className='bg-black text-white'>
-      <div className='container flex justify-between gap-6 items-center flex-col sm:flex-row'>
-        <img src="/lv.png" alt="Logo Loja Virtual" width="200"/>
+    <footer className="bg-black text-white">
+      <div className="container flex justify-between gap-6 items-center flex-col sm:flex-row">
+        <img src="/lv.png" alt="Logo Loja Virtual" width="200" />
         <div>
           <p>&copy; 2024 Loja Virtual. Todos os direitos reservados.</p>
         </div>
-        <div className='text-center space-y-4'>
+        <div className="text-center space-y-4">
           <h5>Social:</h5>
-          <div className='flex space-x-2'>
-          <a href="https://www.facebook.com/"><img src="/social/facebook.webp" alt="Facebook logo" width="30"/></a>
-          <a href="https://www.instagram.com/"><img src="/social/instagram.webp" alt="Instagram logo" width="30"/></a>
-          <a href="https://www.tiktok.com/"><img src="/social/tiktok.webp" alt="Tiktok Logo" width="30"/></a>
+          <div className="flex space-x-2">
+            <a href="https://www.facebook.com/">
+              <img src="/social/facebook.webp" alt="Facebook logo" width="30" />
+            </a>
+            <a href="https://www.instagram.com/">
+              <img
+                src="/social/instagram.webp"
+                alt="Instagram logo"
+                width="30"
+              />
+            </a>
+            <a href="https://www.tiktok.com/">
+              <img src="/social/tiktok.webp" alt="Tiktok Logo" width="30" />
+            </a>
           </div>
         </div>
         <div>
@@ -22,5 +32,5 @@ export default function Footer() {
         </div>
       </div>
     </footer>
-  )
+  );
 }
